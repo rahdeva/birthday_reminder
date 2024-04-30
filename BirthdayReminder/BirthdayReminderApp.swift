05c03c53e2c2559fb6e6d6ec369615fff6067a18
Ngurah Deva <wirandanaputra@gmail.com>
2024-04-30 16:29:12 +0700

    Initial Commit

---
+++ BirthdayReminder/BirthdayReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdayReminderApp.swift
+//  BirthdayReminder
+//
+//  Created by MacBook Air on 30/04/24.
+//
+
+import SwiftUI
+
+@main
+struct BirthdayReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
